7021adc97e226c221a87734337daa15ba4e67ff4
Oliv-S <36530788@users.noreply.github.com>
2018-08-20 23:45:25 +0200

    To standalone class

--- src/SimpleHttpServer.java
+++ src/SimpleHttpServer.java
@@ -4,59 +4,111 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.*;
+import java.net.BindException;
 import java.net.InetSocketAddress;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class SimpleHttpServer {
 
-    public static void main(String[] args) throws Exception {
-        if (args.length<1){
-            System.out.println("Usage: SimpleHttpServer port\r\nExample: SimpleHttpServer 8080");
-            return;
+    private static volatile SimpleHttpServer instance;
+
+    private HttpServer server;
+    private boolean started=false;
+    private String body;
+
+    private SimpleHttpServer() {
+
+    }
+
+
+    public static SimpleHttpServer getInstance() {
+        if(instance == null) {
+            synchronized(SimpleHttpServer.class) {
+                if(instance == null) {
+                    instance = new SimpleHttpServer();
+                }
+            }
         }
-        System.out.println("Server is ready:\r\n");
-        HttpServer server = HttpServer.create(new InetSocketAddress(Integer.parseInt(args[0])), 0);
-        server.createContext("/", new MyHandler());
-        server.setExecutor(null); // creates a default executor
-        server.start();
+        return instance;
+    }
+
+    public boolean start(int port){
+        if (port<1||port>65535) return false;
+        this.started=true;
+        try {
+            this.server = HttpServer.create();
+            server.bind(new InetSocketAddress(port),0);
+            server.createContext("/", new MyHandler());
+            server.setExecutor(null); // creates a default executor
+            server.start();
+
+        }catch (BindException e){
+            //the server cannot bind to the requested address or if the server is already bound.
+            this.started=false;
+            System.out.println("the server cannot bind to the requested address or if the server is already bound. " +e.getMessage() );
+        }catch (NullPointerException e){
+            //addr is null
+            System.out.println("addr is null. " +e.getMessage() );
+        }catch (IllegalArgumentException e){
+            //the port parameter is outside the specified range of valid port values
+            this.started=false;
+            System.out.println("the port parameter is outside the specified range of valid port values. " +e.getMessage() );
+        }catch (IllegalStateException e){
+            this.started=false;
+            System.out.println("the server is already started. " +e.getMessage() );
+        }catch (IOException e){
+            this.started=false;
+            System.out.println(e.getMessage() );
+        }finally {
+            if (!started)
+                server=null;
+        }
+        return started;
+    }
+
+    public boolean isStarted(){
+        return started;
+    }
+
+    public void stop(){
+        server.stop(0);
+        server=null;
+        started = false;
     }
 
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
+            LinkedHashMap<String,List<String>> requestParameters = new LinkedHashMap<>();
+
+            //Save request & connection parameters
+            requestParameters.put("Remote IP", Collections.singletonList(t.getRemoteAddress().toString()));
+            requestParameters.put("URL: " , Collections.singletonList( t.getRequestURI().toString()));
+            requestParameters.put("Protocol: " , Collections.singletonList( t.getProtocol()));
+            requestParameters.put("Method: " , Collections.singletonList( t.getRequestMethod()));
+
             Headers headers = t.getRequestHeaders();
-
-            Set set = headers.entrySet();
-
-            StringBuilder sb = new StringBuilder();
-            sb.append("Client IP: " + t.getRemoteAddress()+"\r\n");
-            sb.append("Server IP: " + t.getLocalAddress()+"\r\n");
-            sb.append("URL: " + t.getRequestURI()+"\r\n");
-            sb.append("Protocol: " + t.getProtocol()+"\r\n");
-            sb.append("Method: " + t.getRequestMethod()+"\r\n");
-            String requestBody = new BufferedReader(new InputStreamReader(t.getRequestBody()))
-                    .lines().collect(Collectors.joining("\n"));
-            if (requestBody.trim().length()>0)
-                sb.append("RequestBody: " + requestBody +"\r\n");
-
-            Iterator iterator = set.iterator();
-            while(iterator.hasNext()){
-                Map.Entry entry = (Map.Entry) iterator.next();
-                sb.append(entry.getKey() + " = " + entry.getValue());
-                sb.append("\r\n");
+            //Save request headers
+            Set<Map.Entry<String, List<String>>> set = headers.entrySet();
+            for (Map.Entry<String, List<String>> entry : set) {
+                requestParameters.put(entry.getKey(),  entry.getValue());
             }
-            System.out.println(sb.toString());
+
+            //Save request Body
+            String requestBody = new BufferedReader(new InputStreamReader(t.getRequestBody()))
+                    .lines().collect(Collectors.joining("\n")).trim();
+
+            System.out.println(requestParameters);
             System.out.println("**********************************\r\n");
-            String response = "This is the response: \r\n" + sb.toString();
+            String response = requestParameters.toString()+
+                    "\r\n**********************************\r\n"+
+                    requestBody;
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
-
         }
     }
 }
